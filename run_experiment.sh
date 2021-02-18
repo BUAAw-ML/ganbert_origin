5ee90f1f0e86e2cd900411e7040599a5b0da715a
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-18 11:04:04 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -36,20 +36,20 @@ python -u ganbert.py \
         --do_lower_case=false \
         --output_dir=ganbert_output_model
 
-#python -u bert.py \
-#        --task_name=QC-fine \
-#        --label_rate=${LABEL_RATE} \
-#        --do_train=true \
-#        --do_eval=true \
-#        --do_predict=false \
-#        --data_dir=${cur_dir} \
-#        --vocab_file=$BERT_BASE_DIR/vocab.txt \
-#        --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-#        --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-#        --max_seq_length=${SEQ_LEN} \
-#        --train_batch_size=${BS} \
-#        --learning_rate=${LR} \
-#        --num_train_epochs=${EPOCHS} \
-#        --warmup_proportion=0.1 \
-#        --do_lower_case=false \
-#        --output_dir=bert_output_model
+python -u bert.py \
+        --task_name=QC-fine \
+        --label_rate=${LABEL_RATE} \
+        --do_train=true \
+        --do_eval=true \
+        --do_predict=false \
+        --data_dir=${cur_dir} \
+        --vocab_file=$BERT_BASE_DIR/vocab.txt \
+        --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+        --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+        --max_seq_length=${SEQ_LEN} \
+        --train_batch_size=${BS} \
+        --learning_rate=${LR} \
+        --num_train_epochs=${EPOCHS} \
+        --warmup_proportion=0.1 \
+        --do_lower_case=false \
+        --output_dir=bert_output_model
